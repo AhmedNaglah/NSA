7b4322e920eeef9b6c55d441ea29daf3bfdc5ca7
Naglah <ahmed.naglah@ufl.edu>
2023-01-31 15:01:40 -0500

    update flask port

--- run.py
+++ run.py
@@ -137,5 +137,4 @@ def reload():
         return app.redirect(url_for("pathost_modular", compartment_id=int(compartment_id)+10, gene_id=int(gene_id)+10))
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1:4000")
-
+    app.run(debug=True, host="0.0.0.0", port=3000)
